df7d6a6c01383721c425856c7bd70faa111e8b4b
hhu <jb>
2021-01-01 17:09:47 +0530

    hi

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('title'),
+          backgroundColor: Colors.blue,
+        ),
+        body:ListView(
+
+
+        )
+
+
+      )
+    );
+  }
+}
